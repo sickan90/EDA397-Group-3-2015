f74ae56bb6e1330b49c4892ce6a1defd22f3e2df
Markus Ekstrom <marekst@student.chalmers.se>
2015-04-21 16:40:49 +0200

    Showing git conflicts 2

--- app/src/main/java/com/github/sickan90/eda397ppapp/MainActivity.java
+++ app/src/main/java/com/github/sickan90/eda397ppapp/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends ActionBarActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
-            //change
+            //somethingdifferent
         }
 
         return super.onOptionsItemSelected(item);
