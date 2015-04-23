dd55070d6304b73cc8d27a65d7d3884b66efbfe0
Markus Ekstrom <marekst@student.chalmers.se>
2015-04-23 09:53:53 +0200

    First connection to pivotal tracker
    Implemented a button which fetches the user stories from pivotal tracker
    and prints the stories as a string in the log window of android studio

--- app/src/main/java/com/github/sickan90/eda397ppapp/MainActivity.java
+++ app/src/main/java/com/github/sickan90/eda397ppapp/MainActivity.java
@@ -1,9 +1,32 @@
 package com.github.sickan90.eda397ppapp;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.StringRequest;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,8 +35,64 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button btnTestLogin = (Button) findViewById(R.id.testLoginButton);
+
+        View.OnClickListener clickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                testLogin();
+            }
+        };
+
+        btnTestLogin.setOnClickListener(clickListener);
+
     }
 
+    public void testLogin() {
+        RequestQueue mRequestQueue;
+
+// Instantiate the cache
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+
+// Set up the network to use HttpURLConnection as the HTTP client.
+        Network network = new BasicNetwork(new HurlStack());
+
+// Instantiate the RequestQueue with the cache and network.
+        mRequestQueue = new RequestQueue(cache, network);
+
+// Start the queue
+        mRequestQueue.start();
+
+        String url ="https://www.pivotaltracker.com/services/v5/projects/1310422/stories";
+
+// Formulate the request and handle the response.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.i("EDA397", "flajsdflk");
+                        Log.i("EDA397", response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Handle error
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String, String> params = new HashMap<String, String>();
+                params.put("X-TrackerToken", "insertyourtokenhere");
+
+                return params;
+            }
+        };
+
+// Add the request to the RequestQueue.
+        mRequestQueue.add(stringRequest);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
