ddedba8942b76e81e3361645473ba09a135cbedd
Markus Ekstrom <marekst@student.chalmers.se>
2015-04-28 10:04:32 +0200

    Created requester singleton

---
+++ app/src/main/java/com/github/sickan90/eda397ppapp/RemoteRequester.java
@@ -0,0 +1,14 @@
+package com.github.sickan90.eda397ppapp;
+
+public class RemoteRequester {
+    private static RemoteRequester ourInstance = new RemoteRequester();
+
+    public static RemoteRequester getInstance() {
+        return ourInstance;
+    }
+
+    private RemoteRequester() {
+    }
+
+    public void sendJsonRequest(String url, )
+}
