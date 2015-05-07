6909e18fdd1ebfa93e45329fdd489326b9298889
Erik Sikander <erik.sikander@gmail.com>
2015-05-07 09:51:09 +0200

    Timer, added integear.xml and more progress

--- app/src/main/java/com/github/sickan90/eda397ppapp/TimerActivity.java
+++ app/src/main/java/com/github/sickan90/eda397ppapp/TimerActivity.java
@@ -1,17 +1,67 @@
 package com.github.sickan90.eda397ppapp;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
 
 
-public class TimerActivity extends ActionBarActivity {
+public class TimerActivity extends ActionBarActivity implements SeekBar.OnSeekBarChangeListener {
+
+    public static final String PAIR_PROGRAMMING_TIMER_TIME = "PAIR_PROGRAMMING_TIMER_TIME";
+
+    private SharedPreferences settings;
+    private SharedPreferences.Editor settingsEditor;
+    private int wantedTime;
+
+
+    private TextView wantedTimeTextView, minTime, maxTime, countdownTime;
+    private Button resetButton, startPauseButton;
+    private SeekBar timeSeekBar;
+    private Switch onOffSwitch;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
+
+        settings = getSharedPreferences(getResources().getString(R.string.preference_file_name), 0);
+        settingsEditor = settings.edit();
+        wantedTime = settings.getInt(PAIR_PROGRAMMING_TIMER_TIME, getResources().getInteger(R.integer.timer_default_time));
+
+        // Get views
+        wantedTimeTextView = (TextView) findViewById(R.id.timerWantedTimeTextField);
+        minTime = (TextView) findViewById(R.id.timerMinTimeTextField);
+        maxTime = (TextView) findViewById(R.id.timerMaxTimeTextField);
+        countdownTime = (TextView) findViewById(R.id.timerCountdownTextField);
+        resetButton = (Button) findViewById(R.id.timerResetButton);
+        startPauseButton = (Button) findViewById(R.id.timerStartStopButton);
+        timeSeekBar = (SeekBar) findViewById(R.id.timerSeekBar);
+        onOffSwitch = (Switch) findViewById(R.id.timerOnOffSwitch);
+
+
+
+        // Set init values
+        wantedTimeTextView.setText(wantedTime + "min");
+        countdownTime.setText(wantedTime);
+        minTime.setText("0");
+        maxTime.setText(getResources().getInteger(R.integer.timer_max_time) + "");
+
+        timeSeekBar.setOnSeekBarChangeListener(this);
+
+    }
+
+    @Override
+    protected void onStop() {
+        settingsEditor.putInt(PAIR_PROGRAMMING_TIMER_TIME, wantedTime);
+        settingsEditor.commit();
     }
 
 
@@ -35,5 +85,22 @@ public class TimerActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+        wantedTime = progress;
+        wantedTimeTextView.setText(wantedTime + " min");
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
