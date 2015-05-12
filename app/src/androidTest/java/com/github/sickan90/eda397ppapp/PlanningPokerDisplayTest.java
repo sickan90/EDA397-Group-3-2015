cf5558e588c55afcf0f21f8cd2e8db88897a08b0
Niel <niel.madlani@gmail.com>
2015-05-12 16:20:01 +0200

    tested planningpokerbutton1

--- app/src/androidTest/java/com/github/sickan90/eda397ppapp/PlanningPokerDisplayTest.java
+++ app/src/androidTest/java/com/github/sickan90/eda397ppapp/PlanningPokerDisplayTest.java
@@ -2,6 +2,7 @@ package com.github.sickan90.eda397ppapp;
 
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.InstrumentationTestCase;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.awt.font.TextAttribute;
@@ -9,20 +10,31 @@ import java.awt.font.TextAttribute;
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class PlanningPokerDisplayTest extends ActivityInstrumentationTestCase2<PlanningPokerDisplay> {
+public class PlanningPokerDisplayTest extends ActivityInstrumentationTestCase2<PlanningPoker> {
 
-    PlanningPokerDisplay activity;
+    PlanningPoker activity;
     public PlanningPokerDisplayTest() {
-        super(PlanningPokerDisplay.class);
+        super(PlanningPoker.class);
     }
 
     protected void setUp() throws Exception{
         super.setUp();
         activity = getActivity();
     }
-    public void testTextViewNotNull() {
-        TextView textView = (TextView) activity.findViewById(R.id.planningPokerDisplayTextView);
-        assertNotNull(textView);
+
+//    public void testTextViewNotNull() {
+//        TextView textView = (TextView) activity.findViewById(R.id.planningPokerDisplayTextView);
+//        assertNotNull(textView);
+//    }
+
+    public void testDisplayCorrectNumber(){
+
+        String button = "0";
+        Button pressNumberButton = (Button) activity.findViewById(R.id.planningPokerButton1);
+        String actual = pressNumberButton.getText().toString();
+        assertEquals(button, actual);
 
     }
+
+
 }
