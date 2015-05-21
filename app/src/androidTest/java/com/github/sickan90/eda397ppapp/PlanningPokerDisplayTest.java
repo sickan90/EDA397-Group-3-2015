a91df91c184bbe722ebd28f343bcc5dfb7cdca7a
MuhanadNabeel <Muhanad_88@hotmail.se>
2015-05-21 13:18:35 +0200

    did some cleaning to the code

--- app/src/androidTest/java/com/github/sickan90/eda397ppapp/PlanningPokerDisplayTest.java
+++ app/src/androidTest/java/com/github/sickan90/eda397ppapp/PlanningPokerDisplayTest.java
@@ -2,17 +2,8 @@ package com.github.sickan90.eda397ppapp;
 
 import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
-import android.support.v4.app.FragmentActivity;
-import android.test.InstrumentationTestCase;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import java.awt.font.TextAttribute;
-import java.net.Authenticator;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -20,7 +11,6 @@ import java.net.Authenticator;
 public class PlanningPokerDisplayTest extends ActivityInstrumentationTestCase2<PlanningPoker> {
 
     private PlanningPoker planningPokerActivity;
-    //private PlanningPokerDisplay planningPokerDisplayActivity;
     private Button pressNumberButton;
 
     public PlanningPokerDisplayTest() {
@@ -30,28 +20,28 @@ public class PlanningPokerDisplayTest extends ActivityInstrumentationTestCase2<P
     protected void setUp() throws Exception{
         super.setUp();
         planningPokerActivity = getActivity();
-
-
-
     }
+
+    /**
+     * Testing: Comparing the press button from the planningPoker and receiving the correct output.
+     */
     public void testDisplayCorrectNumber(){
 
         Instrumentation.ActivityMonitor activityMonitor = getInstrumentation().addMonitor(PlanningPokerDisplay.class.getName(), null, false);
-
         String button = "0";
         pressNumberButton = (Button) planningPokerActivity.findViewById(R.id.planningPokerButton1);
         String actual = pressNumberButton.getText().toString();
         assertEquals(button, actual);
 
-
         getActivity().runOnUiThread(new Runnable() {
             @Override
             public void run() {
+                // Pressing the chosen number ("0")
                 pressNumberButton.performClick();
             }
         });
-
-        PlanningPokerDisplay planningPokerDisplay = (PlanningPokerDisplay) getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 10000);
+        //stating the new activity displaying the chosen number
+        PlanningPokerDisplay planningPokerDisplay = (PlanningPokerDisplay) getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 50000);
         assertNotNull(planningPokerDisplay);
         TextView textView = (TextView) planningPokerDisplay.findViewById(R.id.planningPokerDisplayTextView);
         assertNotNull(textView);
